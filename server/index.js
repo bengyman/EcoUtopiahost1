abb9ca0dcaa9f63597dbd858e44d1523d7dc64a1
VoidCodes <234273T@mymail.nyp.edu.sg>
2024-06-07 14:47:35 +0800

    server init

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const db = require('./models');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+// Enable CORS
+app.use(cors({
+    origin: process.env.CLIENT_URL
+}));
+
+// Simple Route
+app.get("/", (req, res) => {
+    res.send("EcoUtiopia API");
+});
+
+// Routes
+/*const tutorialRoute = require('./routes/tutorial');
+app.use("/tutorial", tutorialRoute);*/
+
+db.sequelize.sync({ alter: true }).then(() => {
+    let port = process.env.APP_PORT;
+
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}).catch((err) => {
+    console.log(err);
+});
