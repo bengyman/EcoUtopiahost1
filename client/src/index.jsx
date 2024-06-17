3ea85eef0629258294ebf7cdce5f363f8005a3a2
VoidCodes <234273T@mymail.nyp.edu.sg>
2024-06-18 01:32:31 +0800

    refactor: Update Mantine theme and component styles

---
+++ client/src/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import '@mantine/core/styles.css';
+import { MantineProvider, createTheme, rem } from '@mantine/core'
+import App from './pages/App.jsx'
+
+const theme = createTheme({
+  //primaryColor: 'violet'
+  colors: {
+    /* Add your custom colors here */
+    deepBlue: [
+      '#eef3ff',
+      '#dce4f5',
+      '#b9c7e2',
+      '#94a8d0',
+      '#748dc1',
+      '#5f7cb8',
+      '#5474b4',
+      '#44639f',
+      '#39588f',
+      '#2d4b81',
+    ],
+  },
+
+  shadows: {
+    md: '1px 1px 3px rgba(0, 0, 0, .25)',
+    xl: '5px 5px 3px rgba(0, 0, 0, .25)',
+  },
+
+  headings: {
+    fontFamily: 'Roboto, sans-serif',
+    sizes: {
+      h1: { fontSize: rem(36) },
+    },
+  },
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <MantineProvider theme={theme} defaultColorScheme='auto'>
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
+)
