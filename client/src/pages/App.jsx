d63e0e9f767ddd2e932ef157fee126f19f67ec6a
VoidCodes <234273T@mymail.nyp.edu.sg>
2024-06-19 01:23:58 +0800

    refactor: Update App component to use Mantine UI library

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -1,13 +1,17 @@
-import { Button, Container, Group } from "@mantine/core"
+import { Button, Container, Group, Anchor } from "@mantine/core"
 
 function App() {
   return (
-    <Container>
-      <h1>Hello, Mantine!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <Container size={"xl"}>
+      <h1>EcoUtopia Home Page</h1>
+      <p>Welcome to EcoUtopia home page. This page is under construction. There are buttons below for navigation.</p>
       <Group>
-        <Button color='deepBlue'>Click me</Button>
-        <Button color='red'>Click me</Button>
+        <Anchor href="/test">
+          <Button color='deepBlue'>Click me</Button>
+        </Anchor>
+        <Anchor href="/courses">
+          <Button color='red'>Click me</Button>
+        </Anchor>
         <Button color='green'>Click me</Button>
       </Group>
     </Container>
