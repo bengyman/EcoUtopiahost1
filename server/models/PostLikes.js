f65b4e5443b0971122754ae30b14a28af03adcd3
VoidCodes <234273T@mymail.nyp.edu.sg>
2024-08-10 01:39:49 +0800

    Add PostLikes model and associations

---
+++ server/models/PostLikes.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+    const PostLikes = sequelize.define('PostLikes', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        postId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'Posts',
+                key: 'id'
+            }
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'Users',
+                key: 'id'
+            }
+        }
+    });
+
+    return PostLikes;
+};
