e19bba6fc38ebe04a36c09a4dd98ee599d58e358
VoidCodes <234273T@mymail.nyp.edu.sg>
2024-08-10 01:40:38 +0800

    Refactor Orders model associations for better data integrity

--- server/models/Orders.js
+++ server/models/Orders.js
@@ -1,36 +1,38 @@
 module.exports = (sequelize, DataTypes) => {
     const Orders = sequelize.define('Orders', {
-        order_id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        resident_id: {
-            //call from resident table as foreign key
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        course_id: {
-            //call from course table as foreign key
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        order_date: {
-            type: DataTypes.DATE,
-            allowNull: false
-        },
-        order_status: {
-            type: DataTypes.ENUM('Upcoming', 'Completed', 'Refunded', 'Pending'),
-            allowNull: false
-        }
-        
+      order_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      resident_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      course_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      order_date: {
+        type: DataTypes.DATE,
+        allowNull: false
+      },
+      order_status: {
+        type: DataTypes.ENUM('Upcoming', 'Completed', 'Refunded', 'Pending'),
+        allowNull: false
+      },
+      payment_intent: {
+        type: DataTypes.STRING, 
+        allowNull: true
+      }
+
     }, {
-        tableName: 'orders',
+      tableName: 'orders',
     });
     Orders.associate = (models) => {
-        Orders.belongsTo(models.Resident, { foreignKey: 'resident_id' });
-        Orders.belongsTo(models.Course, { foreignKey: 'course_id' });
+      Orders.belongsTo(models.Resident, { foreignKey: 'resident_id' });
+      Orders.belongsTo(models.Course, { foreignKey: 'course_id' });
     };
-
-    return Orders;
-}
+  
+    return Orders;
+  };
