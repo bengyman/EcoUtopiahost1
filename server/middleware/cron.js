e5edc64fd2ce6e37d8a7c8b0cfefb96db16a93a8
JustJJJCCC1 <idkjjjccc0478@gmail.com>
2024-08-09 13:23:22 +0800

    added more payment methods

--- server/middleware/cron.js
+++ server/middleware/cron.js
@@ -1,10 +1,9 @@
-// middleware/cron.js
 const cron = require('node-cron');
-const { Orders, Course } = require('../models'); // Adjust the path as needed
+const { Orders, Course, sequelize } = require('../models'); // Adjust the path as needed
 const { Op } = require('sequelize');
 
-// Schedule a job to run every minute
-cron.schedule('* * * * *', async () => {
+// Schedule a job to run every 5 seconds
+cron.schedule('*/1 * * * * *', async () => {
   try {
     // Get the current date
     const currentDate = new Date();
@@ -29,7 +28,30 @@ cron.schedule('* * * * *', async () => {
       order.order_status = 'Completed';
       await order.save();
     }
+
+    // Check for duplicate orders based on payment_intent
+    const duplicates = await Orders.findAll({
+      attributes: ['payment_intent'],
+      group: ['payment_intent'],
+      having: sequelize.literal('COUNT(*) > 1')
+    });
+
+    for (const duplicate of duplicates) {
+      // Find all orders that match the duplicate payment_intent
+      const duplicateOrders = await Orders.findAll({
+        where: {
+          payment_intent: duplicate.payment_intent
+        },
+        order: [['createdAt', 'ASC']] // Keeps the oldest record
+      });
+
+      // Delete all but the first order (oldest)
+      for (let i = 1; i < duplicateOrders.length; i++) {
+        await duplicateOrders[i].destroy();
+      }
+    }
+
   } catch (error) {
-    console.error('Error updating orders:', error);
+    console.error('Error updating orders or checking duplicates:', error);
   }
 });
--- server/routes/payment.js
+++ server/routes/payment.js
@@ -17,7 +17,7 @@ router.post('/create-checkout-session', authenticateToken, async (req, res) => {
 
     try {
         const session = await stripe.checkout.sessions.create({
-            payment_method_types: ['card'],
+            payment_method_types: ['card', 'alipay', 'paynow'],
             line_items: items.map((item) => ({
                 price_data: {
                     currency: 'sgd',
