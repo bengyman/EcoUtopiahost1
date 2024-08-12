c849bb42346062d806ca3941ab56ca6d81b1987f
Kaushik <fubukishirou430@gmail.com>
2024-08-12 13:50:53 +0800

    new post model

--- server/models/Post.js
+++ server/models/Post.js
@@ -38,9 +38,17 @@ module.exports = (sequelize) => {
             type: DataTypes.INTEGER,
             allowNull: true,
         },
-        residentName: {
+        instructor_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        residentName: {  // Make residentName nullable
             type: DataTypes.STRING,
-            allowNull: false,
+            allowNull: true,
+        },
+        name: {  // Add new column for name
+            type: DataTypes.STRING,
+            allowNull: true,  // Adjust according to your needs
         },
         commentsCount: {
             type: DataTypes.VIRTUAL,
@@ -73,6 +81,7 @@ module.exports = (sequelize) => {
 
     Post.associate = function (models) {
         Post.belongsTo(models.Resident, { foreignKey: 'resident_id' });
+        Post.belongsTo(models.Instructor, { foreignKey: 'instructor_id' });
         Post.hasMany(models.Comment, { foreignKey: 'post_id', onDelete: 'CASCADE' });
         Post.belongsToMany(models.User, { through: 'PostLikes', as: 'likedByUsers', foreignKey: 'postId' });
     };
